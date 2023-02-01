340ca29fdfd7c01823bc6205cc8cff8e22decf63
andrew-bierman <abbierman101@gmail.com>
2023-01-31 22:47:02 -0500

    got dark mode comp working on this branch

---
+++ react-app/src/components/DarkModeToggle/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Darkreader, { Switch, useDarkreader } from 'react-darkreader';
+
+const DarkModeToggle = () => {
+    const [isDark, { toggle }] = useDarkreader(false, {
+        brightness: 120,
+        contrast: 95,
+        sepia: 0,
+        },
+        {
+            ignoreImageAnalysis: ['.background-image', '.background-image-2', '.login-page-splash-image-container']}
+    );
+
+    const [message, setMessage] = useState('Current theme mode is what');
+
+    return (
+        <>
+            <Switch
+                checked={isDark}
+                onChange={toggle}
+                styling="github"
+            />
+            {/* <Darkreader
+                defaultDarken
+                theme={{
+                    brightness: 100,
+                    contrast: 95,
+                    sepia: 0,
+                }}
+                styling="github"
+                onChange={isDark =>
+                    setMessage(`Current theme mode is ${isDark ? '🌜' : '🌞'}`)
+                }
+            /> */}
+        </>
+    )
+};
+
+export default DarkModeToggle;
