03504e2a28d2c07ee300103d37181c8f4d3f1f32
andrew-bierman <abbierman101@gmail.com>
2023-02-01 16:11:10 -0500

    progress on news

---
+++ react-app/src/components/Profile/ProfileNewsFeed.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { openInNewTab } from "../utility";
+import { getAllNewsForHomePage, getUserBookmarkedNews } from "../../store/news";
+import NewsCard from "../NewsCard";
+
+// import "./HomePageNewsFeed.css";
+
+
+const ProfileNewsFeed = () => {
+    const dispatch = useDispatch();
+
+    const [loading, setLoading] = useState(true);
+
+    const userNews = useSelector((state) => state.news.userNews);
+
+    const formatArticles = userNews?.map((article) => {
+        const { title, summary, url, symbol, image_link, sourc } = article;
+        return {
+            ...article,
+            url: article.url,
+            title: article.title,
+            source: article.source,
+            summary: article.summary,
+            ticker_sentiment: [{ticker: article.symbol}],
+            banner_image: article.image_link,
+        }
+    })
+
+
+    useEffect(() => {
+        setLoading(true);
+        dispatch(getUserBookmarkedNews())
+            .then(() => {
+                setLoading(false);
+            }
+            );
+    }, []);
+
+    return (
+        <div className="news-feed-container">
+            <div className="news-feed-header">
+                <h2>Bookmarked News</h2>
+            </div>
+            <>
+                {
+                    (!loading && userNews?.length > 0) && formatArticles.map((article, idx) => {
+                        return (
+                            <NewsCard article={article} key={idx} />
+                        )
+                    })
+                }
+            </>
+        </div>
+    );
+}
+
+export default ProfileNewsFeed;
